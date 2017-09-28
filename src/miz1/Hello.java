c9b78f8dd2e562ffcb2e85450247f3f0291c741f
miza <mizzzamidin@gmail.com>
2017-09-28 18:43:01 +0600

    ушинши коммит

--- src/miz1/Hello.java
+++ src/miz1/Hello.java
@@ -5,5 +5,11 @@ public class Hello {
         System.out.println("Hello World!");
         System.out.println("ул зертханалык жумыстын максаты:");
         System.out.println("GitGub-пен жумыс жасап уйрену.");
+
+        int a = MyMath.kvadrat(5);
+        int b = MyMath.kub(5);
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
